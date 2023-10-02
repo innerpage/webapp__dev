ea358e7505287f08e2143ae3db6ecd77c194f873
Projckt <projckt@protonmail.com>
2023-10-02 23:19:19 +0530

    fixes

--- app-frontend/src/components/views/settings/v-delete-account/v-delete-account.tsx
+++ app-frontend/src/components/views/settings/v-delete-account/v-delete-account.tsx
@@ -10,7 +10,7 @@ export class VDeleteAccount {
     return (
       <Host>
         <e-text variant="display">Are you sure?</e-text>
-        <e-text>You account and data will be lost forever. Deleting your account is an irreversible step.</e-text>
+        <e-text>You account and data will be lost forever. Deleting your account is an irreversible step</e-text>
         <l-spacer value={0.5}></l-spacer>
         <e-button action="deleteAccount" theme="danger">
           Delete my account
