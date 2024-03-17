db01c6f4f2ab8ac72e365231189c686e3cc34c26
Projckt <projckt@protonmail.com>
2024-03-17 22:05:14 +0530

    fixes

--- frontend/src/components/views/app/v-home/v-home.tsx
+++ frontend/src/components/views/app/v-home/v-home.tsx
@@ -19,7 +19,15 @@ export class VHome {
           <p-sidebar></p-sidebar>
           <c-main>
             {/* TEMPLATE CODE Starts */}
-            <div class="box-full"></div>
+            <e-text variant="display">Overview</e-text>
+            <div class="card card-full"></div>
+            <l-spacer value={2}></l-spacer>
+            <l-row justifyContent="space-between">
+              <div class="card card-quarter"></div>
+              <div class="card card-quarter"></div>
+              <div class="card card-quarter"></div>
+              <div class="card card-quarter"></div>
+            </l-row>
             {/* TEMPLATE CODE Ends */}
           </c-main>
         </c-content-area>
