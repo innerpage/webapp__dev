55e614ab700a4720881b5810b8aba05f50aff80b
Projckt <projckt@protonmail.com>
2024-04-04 09:03:53 +0530

    fixes profile and billing pages

--- frontend/src/components/views/settings/v-billing/v-billing.tsx
+++ frontend/src/components/views/settings/v-billing/v-billing.tsx
@@ -14,19 +14,20 @@ export class VBilling {
   render() {
     return (
       <Host>
-        <p-sidebar></p-sidebar>
-        <c-main>
-          <e-text variant="heading">
-            <l-row align="flex-end">
-              <ph-credit-card color="var(--color__grey--light)"></ph-credit-card>
-              <l-spacer value={0.25} variant="horizontal"></l-spacer>
-              <e-text>Billing</e-text>
-            </l-row>
-          </e-text>
-          <l-spacer value={0.5}></l-spacer>
-          <l-seperator></l-seperator>
-          <l-spacer value={2}></l-spacer>
-        </c-main>
+        <p-topbar></p-topbar>
+        <c-content-area>
+          <p-sidebar></p-sidebar>
+          <c-main>
+            <e-text variant="heading">
+              <l-row align="flex-end">
+                <ph-credit-card color="var(--color__grey--light)"></ph-credit-card>
+                <l-spacer value={0.25} variant="horizontal"></l-spacer>
+                <e-text>Billing</e-text>
+              </l-row>
+            </e-text>
+            <l-spacer value={1}></l-spacer>
+          </c-main>
+        </c-content-area>
       </Host>
     );
   }
--- frontend/src/components/views/settings/v-profile/v-profile.tsx
+++ frontend/src/components/views/settings/v-profile/v-profile.tsx
@@ -14,50 +14,51 @@ export class VProfile {
   render() {
     return (
       <Host>
-        <p-sidebar></p-sidebar>
-        <c-main>
-          <e-text variant="heading">
-            <l-row>
-              <ph-user color="var(--color__grey--light)" align="flex-end"></ph-user>
-              <l-spacer value={0.25} variant="horizontal"></l-spacer>
-              <e-text>Profile</e-text>
-            </l-row>
-          </e-text>
-          <l-spacer value={0.5}></l-spacer>
-          <l-seperator></l-seperator>
-          <l-spacer value={2}></l-spacer>
-          <c-card>
-            <e-text variant="footnote">NAME</e-text>
-            <p-editable-text type="text" label={state.accountName}></p-editable-text>
+        <p-topbar></p-topbar>
+        <c-content-area>
+          <p-sidebar></p-sidebar>
+          <c-main>
+            <e-text variant="heading">
+              <l-row>
+                <ph-user color="var(--color__grey--light)" align="flex-end"></ph-user>
+                <l-spacer value={0.25} variant="horizontal"></l-spacer>
+                <e-text>Profile</e-text>
+              </l-row>
+            </e-text>
             <l-spacer value={1}></l-spacer>
+            <c-card>
+              <e-text variant="footnote">NAME</e-text>
+              <p-editable-text type="text" label={state.accountName}></p-editable-text>
+              <l-spacer value={1}></l-spacer>
+              <l-seperator></l-seperator>
+              <l-spacer value={1}></l-spacer>
+              <e-text variant="footnote">EMAIL</e-text>
+              <p-editable-text type="link" label={`${state.accountEmail}`}></p-editable-text>
+              <l-spacer value={1}></l-spacer>
+              <l-seperator></l-seperator>
+              <l-spacer value={1}></l-spacer>
+              <e-text variant="footnote">PASSWORD</e-text>
+              <p-editable-text type="text" label="********"></p-editable-text>
+            </c-card>
+            <l-spacer value={2}></l-spacer>
+            <e-button variant="ghost" action="logout">
+              <l-row>
+                <ph-sign-out color="var(--color__blue--regular)"></ph-sign-out>
+                &nbsp;Logout
+              </l-row>
+            </e-button>
+            <l-spacer value={2}></l-spacer>
             <l-seperator></l-seperator>
             <l-spacer value={1}></l-spacer>
-            <e-text variant="footnote">EMAIL</e-text>
-            <p-editable-text type="link" label={`${state.accountEmail}`}></p-editable-text>
-            <l-spacer value={1}></l-spacer>
-            <l-seperator></l-seperator>
-            <l-spacer value={1}></l-spacer>
-            <e-text variant="footnote">PASSWORD</e-text>
-            <p-editable-text type="text" label="********"></p-editable-text>
-          </c-card>
-          <l-spacer value={2}></l-spacer>
-          <e-button variant="ghost" action="logout">
-            <l-row>
-              <ph-sign-out color="var(--color__blue--regular)"></ph-sign-out>
-              &nbsp;Logout
-            </l-row>
-          </e-button>
-          <l-spacer value={2}></l-spacer>
-          <l-seperator></l-seperator>
-          <l-spacer value={1}></l-spacer>
-          <e-link url="/delete-account" theme="danger">
-            <l-row>
-              <ph-trash weight={state.activeView === 'page1' ? 'fill' : 'regular'}></ph-trash>
-              <l-spacer value={0.25} variant="horizontal"></l-spacer>
-              <e-text> Delete your account and data</e-text>
-            </l-row>
-          </e-link>
-        </c-main>
+            <e-link url="/delete-account" theme="danger">
+              <l-row>
+                <ph-trash weight={state.activeView === 'page1' ? 'fill' : 'regular'}></ph-trash>
+                <l-spacer value={0.25} variant="horizontal"></l-spacer>
+                <e-text> Delete your account and data</e-text>
+              </l-row>
+            </e-link>
+          </c-main>{' '}
+        </c-content-area>
       </Host>
     );
   }
