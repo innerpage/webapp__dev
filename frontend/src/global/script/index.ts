16e9eeccc47e5c127a6fac0813b28b153e7ddffe
Projckt <projckt@protonmail.com>
2024-06-09 01:33:46 +0530

    Fixes

--- frontend/src/global/script/index.ts
+++ frontend/src/global/script/index.ts
@@ -1,5 +1,5 @@
-export { state } from './store/store';
-export { Vars } from './vars/vars';
-export { IO } from './socket/io';
-export { initSocket } from './socket/io';
-export { ApiVar } from './vars/ApiVar';
+export { state } from "./store/store";
+export { Vars } from "./vars/vars";
+export { IO } from "./socket/io";
+export { initSocket } from "./socket/io";
+export { ApiVar } from "./vars/ApiVar";
