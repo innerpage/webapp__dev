db574a199f76ed953848cd6b97a5140735f674f3
Projckt <projckt@protonmail.com>
2024-06-16 07:26:30 +0530

    fixes build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
-## Delete old files
+## Delete old prod & ./server/dest
 rm -rf ../prod server/dest
 mkdir ../prod
 
-## Build server
+## Build ./server
 npm run --prefix server build
 rsync -av --delete server/dest/ ../prod
 cp server/{.gitignore,package.json,.env} ../prod
 
-## Build frontend
+## Build ./frontend
 npm run --prefix frontend build 
 rsync -av --delete frontend/www/ ../prod/www
 
-## Init prod
+## Init ../prod
 npm --prefix ../prod install
 
-## Init git in ./prod
+## Init git in ../prod
 git init ../prod
 # git -C ../prod remote add origin git@github.com-{username}:{username}/{repo_name}__prod.git
 git -C ../prod remote add origin git@github.com-projckt:projckt/starter_webapp-prod.git
 
-## Push prod 
+## Push ../prod 
 git -C ../prod add --all
 git -C ../prod commit -m "Deploy build `date`"
 git -C ../prod push origin main --force
