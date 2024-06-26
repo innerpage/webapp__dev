dbd7d81515a06668d8561f67509cc33327f26465
Projckt <projckt@protonmail.com>
2024-06-26 23:24:44 +0530

    comments out path

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import * as path from "path";
+// import * as path from "path";
 
 const app = express();
 app.use(cors());
